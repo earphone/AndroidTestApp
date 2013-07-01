805eea23178cb6d8541163e5a4717efa94d6021f
higashi <bhigashi169@gmail.com>
2013-06-30 20:14:43 -1000

    initial parse commit
    	Parse was first used here to test to make sure that an object was sent to the database.
    	Also, another object was created and then related to the first.
    	Have yet to test the analytics of how many times the app is opened in the parse tutorial.
    
    	Details:
    		Added:	import com.parse.Parse
    			import com.parse.ParseAnalytics
    			parse initialize
    			INTERNET permission
    			ACCESS_NETWORK_STATE permission
    			ParseObject purpleTest
    			ParseObject orangeTest

--- src/com/example/firstapp/DisplayMessageActivity.java
+++ src/com/example/firstapp/DisplayMessageActivity.java
@@ -1,5 +1,8 @@
 package com.example.firstapp;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -17,8 +20,8 @@ public class DisplayMessageActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_display_message);
-
+		setContentView(R.layout.activity_display_message);	        		
+		
 		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
 		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			// Show the Up button in the action bar.
--- src/com/example/firstapp/MainActivity.java
+++ src/com/example/firstapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.firstapp;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,8 +15,33 @@ public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.firstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
+
+       // Parse: Initialize
+    	Parse.initialize(this, "OdAVwKCSwWGzcYYziwk0MxsZgP11D4xYBbD7cdSN", "aMu14e9jrNTV9C1qF62ZV8wRSz7KZe7qffwMG4qX");  
+        
+        // Parse: Used to track how many times app is opened
+        //ParseAnalytics.trackAppOpened(getIntent());
+        
+        // Parse: Test to make sure that the SDK is working properly
+        final ParseObject purpleTest = new ParseObject("purpleNurple");
+        purpleTest.put("herro", "thar");
+        purpleTest.put("foo", "bar");
+        
+        // Parse: Creating a second class for relationship
+        final ParseObject orangeTest = new ParseObject("fiddleHurdle");
+        orangeTest.put("foo", "hot");
+     
+        // Parse: Add a relation between 'purple' and 'orange'
+        orangeTest.put("parent", purpleTest);
+        
+        // Parse: Save both 'purple' and 'orange' and their relations
+        	// by just saving orange which is the dependent one
+        // pupleTest.saveInBackground();
+        orangeTest.saveInBackground();
+       
         setContentView(R.layout.activity_main);
+        
     }
 
 
