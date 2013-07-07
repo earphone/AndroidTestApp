2306f8d633f0da293d4836fd444fc6647c55365b
higashi <bhigashi169@gmail.com>
2013-07-06 17:32:41 -1000

    July 6, 2013

--- gen/com/example/firstapp/R.java
+++ gen/com/example/firstapp/R.java
@@ -24,12 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int RelativeLayout1=0x7f080001;
+        public static final int action_settings=0x7f080008;
+        public static final int button_login=0x7f080006;
         public static final int edit_message=0x7f080000;
+        public static final int forgot_password=0x7f080005;
+        public static final int keepLogged=0x7f080007;
+        public static final int passwordLogin=0x7f080003;
+        public static final int signUp=0x7f080004;
+        public static final int usernameLogin=0x7f080002;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int login=0x7f030002;
     }
     public static final class menu {
         public static final int display_message=0x7f070000;
@@ -41,7 +49,13 @@ public final class R {
         public static final int button_send=0x7f050004;
         public static final int edit_message=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int keep_login=0x7f05000a;
+        public static final int login_button=0x7f050008;
+        public static final int password_forgot=0x7f050009;
+        public static final int password_hint=0x7f050007;
+        public static final int signup_button=0x7f05000b;
         public static final int title_activity_display_message=0x7f050005;
+        public static final int username_hint=0x7f050006;
     }
     public static final class style {
         /** 
