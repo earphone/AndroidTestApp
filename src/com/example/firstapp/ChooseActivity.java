69fdfd248e54236f15e2eda3a633e030ce5c43e6
Brandon Higashi <bhigashi169@gmail.com>
2013-08-25 20:49:40 -1000

    August 25, 2013

--- src/com/example/firstapp/ChooseActivity.java
+++ src/com/example/firstapp/ChooseActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class ChooseActivity extends Activity {
@@ -21,10 +22,28 @@ public class ChooseActivity extends Activity {
 		return true;
 	}
 	
+	// Options for ActionBar
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+    	case R.id.chooserLogout:
+    		// If logout clicked, then logout :p
+    		MainActivity mainAct = new MainActivity();
+    		mainAct.logout();
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+        }
+    }
 	
 	// Sends the user to MainActivity to add a new word to the list
 	public void addWord(View view) {
 		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+	}
+	
+	public void addDate(View view) {
+		Intent intent = new Intent(this,AddDate.class);
 		startActivity(intent);
 	}
 	
